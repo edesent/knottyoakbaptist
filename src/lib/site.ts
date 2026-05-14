ddc5f3caf380064e87491f30364ac50e80dc65fa
jmart2cor515 <jmart2cor515@gmail.com>
2026-05-14 12:57:34 -0400

    Updated Sunday School time from 9:00 AM to 10:00 AM in site config

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -32,7 +32,7 @@ export const site = {
   },
 
   services: [
-    { label: "Sunday School",     time: "9:00 AM",  day: "Sunday",   iso: "09:00" },
+    { label: "Sunday School",     time: "10:00 AM",  day: "Sunday",   iso: "10:00" },
     { label: "Morning Worship",   time: "10:00 AM", day: "Sunday",   iso: "10:00" },
     { label: "Evening Service",   time: "6:00 PM",  day: "Sunday",   iso: "18:00" },
     { label: "Bible Study & Prayer", time: "6:00 PM", day: "Thursday", iso: "18:00" },
