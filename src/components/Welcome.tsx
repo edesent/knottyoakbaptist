ff94dee3c9f7a8eb9ecba7cf80b44059c8b55cc7
jmart2cor515 <jmart2cor515@gmail.com>
2026-05-22 15:13:03 -0400

    Remove "no church background and" from pastor's welcome paragraph

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -47,7 +47,7 @@ export default function Welcome() {
               <p>
                 That teenager was me. Surrounded by familiar places and familiar
                 people, yet inside I was lost, directionless, and spiritually empty.
-                I had no church background and no idea what I was truly missing.
+                I had no idea what I was truly missing.
               </p>
               <p>
                 Then one day, a childhood friend invited me to a teen game night at
