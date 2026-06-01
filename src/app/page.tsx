b7e164c9c963f30e4f8f7317fd7d101f4effde3e
jmart2cor515 <jmart2cor515@gmail.com>
2026-06-01 14:39:37 -0400

    Remove unused MissionQuote import from homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import Welcome from "@/components/Welcome";
 import ServiceTimes from "@/components/ServiceTimes";
-import MissionQuote from "@/components/MissionQuote";
 import Events from "@/components/Events";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
