122464905f8572a957f2d0c44f050807756b7cc0
jmart2cor515 <jmart2cor515@gmail.com>
2026-06-01 14:42:15 -0400

    Remove social icons from bottom of footer (will move up under the verse)

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -75,32 +75,6 @@ export default function Footer() {
           </div>
         </div>
 
-        {/* Social Icons */}
-        <div className="mt-12 flex justify-center gap-5">
-          <a
-            href={site.social.facebook}
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="Knotty Oak Baptist Church on Facebook"
-            className="text-forest-100/50 hover:text-[#1877F2] transition-colors"
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="currentColor">
-              <path d="M24 12.073C24 5.405 18.627 0 12 0S0 5.405 0 12.073C0 18.1 4.388 23.094 10.125 24v-8.437H7.078v-3.49h3.047V9.41c0-3.025 1.792-4.697 4.533-4.697 1.312 0 2.686.236 2.686.236v2.97h-1.513c-1.491 0-1.956.93-1.956 1.883v2.271h3.328l-.532 3.49h-2.796V24C19.612 23.094 24 18.1 24 12.073z"/>
-            </svg>
-          </a>
-          <a
-            href={site.social.youtube}
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="Knotty Oak Baptist Church on YouTube"
-            className="text-forest-100/50 hover:text-[#FF0000] transition-colors"
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="currentColor">
-              <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/>
-            </svg>
-          </a>
-        </div>
-
         <div className="mt-8 pt-8 border-t border-forest-100/10 flex flex-col md:flex-row items-center justify-between gap-3 text-xs text-forest-100/50">
           <p>&copy; {new Date().getFullYear()} {site.name}. All are welcome.</p>
           <p className="tracking-[0.2em] uppercase">{site.tagline}</p>
